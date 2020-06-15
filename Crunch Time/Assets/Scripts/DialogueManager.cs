9d5e8bf54efed88afbb8fef1d525250643f580e8
Chris <cwaites@hotmail.com>
2020-06-16 09:36:28 +1000

    Initial prefab creation

---
+++ Crunch Time/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+
+    private Queue<string> sentences;
+
+    public Animator dialogueBoxAnimator;
+    public Animator npcFaceAnimator;
+
+    public List<GameObject> talkButtons;
+
+    bool isTalking;
+
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+        isTalking = false;
+    }
+
+
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        foreach (GameObject talkButton in talkButtons)
+        {
+            talkButton.gameObject.SetActive(false);
+        }
+        dialogueBoxAnimator.SetBool("IsOpen", true);
+        npcFaceAnimator.SetBool("IsOpen", true);
+        isTalking = true;
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+
+        DisplayNextSentence();
+
+
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (isTalking)
+        {
+            if (sentences.Count == 0)
+            {
+                EndDialogue();
+                return;
+            }
+
+            string sentence = sentences.Dequeue();
+            StopAllCoroutines();
+            StartCoroutine(TypeSentence(sentence));
+        }
+
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        foreach (GameObject talkButton in talkButtons)
+        {
+
+        }
+        dialogueBoxAnimator.SetBool("IsOpen", false);
+        npcFaceAnimator.SetBool("IsOpen", false);
+        isTalking = false;
+    }
+}
---
+++ Crunch Time/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    //public Animator animator;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        //animator.SetFloat("RightSpeed", movement.x);
+        //animator.SetFloat("UpSpeed", movement.y);
+    }
+
+    void Movement()
+    {
+        //movement
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+}
